df6f2856f5f5da0f64dcede71858f553dbeb1b23
ChangYu <changyu@MacBook.local>
2026-03-28 22:30:17 +0800

    feat: add memory updated

--- src/main/java/com/changyu496/agent/mini/Main.java
+++ src/main/java/com/changyu496/agent/mini/Main.java
@@ -14,25 +14,39 @@ import java.util.Scanner;
 public class Main {
 
 
+    private static final int MAX_MESSAGE_SIZE = 20;
+
     public static void main(String[] args) {
 
         List<Message> historyMessages = new ArrayList<>();
-
+        Message system = new Message();
+        system.setRole("system");
+        system.setContent("你是一个mini助手，帮助用户解决问题");
+        historyMessages.add(system);
+        Scanner scanner = new Scanner(System.in);
+        OpenAIHttpClient client = new OpenAIHttpClient();
         while (true) {
-            Scanner scanner = new Scanner(System.in);
             System.out.print("mini agent >> ");
             String userInput = scanner.nextLine();
             if ("exit".equals(userInput) || "q".equals(userInput)){
                 break;
             }
-            OpenAIHttpClient client = new OpenAIHttpClient();
             Message message = new Message();
             message.setContent(userInput);
             message.setRole("user");
             historyMessages.add(message);
-            OpenAIResponse openAIResponse = client.call(historyMessages);
-            historyMessages.add(openAIResponse.getChoices().get(0).getMessage());
-            System.out.println("assistant:" + openAIResponse.getChoices().get(0).getMessage().getContent());
+            if (historyMessages.size()>=MAX_MESSAGE_SIZE){
+                // 永远保留第一个system prompt
+                historyMessages.remove(1);
+            }
+            try {
+                OpenAIResponse openAIResponse = client.call(historyMessages);
+                historyMessages.add(openAIResponse.getChoices().get(0).getMessage());
+                System.out.println("assistant:" + openAIResponse.getChoices().get(0).getMessage().getContent());
+            }catch (Exception e){
+                System.out.println("大模型调用异常，请稍后重试");
+            }
+
         }
 
 
